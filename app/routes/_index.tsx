6840415ba3f542f68054b9270e2da6e1c6b75a75
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-06 16:52:53 -0500

    feat: prettier reorder tailwind CSS classes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,32 @@
 export default function Index() {
   return (
     <main>
-      <h1 className="text-green font-normal">Welcome to Remix</h1>
+      <h1 className="font-normal text-green">Welcome to Remix</h1>
       <ul>
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer noopener">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer noopener"
+          >
             15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer noopener">
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer noopener"
+          >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer noopener">
+          <a
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer noopener"
+          >
             Remix Docs
           </a>
         </li>
