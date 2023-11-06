de641c42fa70724bc3bd999c655aede840486773
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-06 14:19:47 -0500

    feat: add remix CSS bundler

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
 import type { LinksFunction } from '@remix-run/node';
+import { cssBundleHref } from '@remix-run/css-bundle';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 import { withDevTools } from 'remix-development-tools';
@@ -17,14 +18,13 @@ export const links: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@600&display=swap',
   },
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
   ...(process.env.NODE_ENV === 'development' ? [{ rel: 'stylesheet', href: rdtStylesheet }] : []),
 ];
 
 export const meta: MetaFunction = () => [
   {
-    charset: 'utf-8',
     title: 'Climate United',
-    viewport: 'width=device-width,initial-scale=1',
   },
 ];
 
@@ -40,6 +40,8 @@ function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
