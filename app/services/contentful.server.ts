91032b54531a272031436cf7659b4a10bb0c231a
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-06 12:14:49 -0500

    feat: add theme initial theme colors, Contenful service

---
+++ app/services/contentful.server.ts
@@ -0,0 +1,12 @@
+export function fetchGraphQL(query: string, variables = {}, preview = false): Promise<any> {
+  return fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${
+        preview ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN
+      }`,
+    },
+    body: JSON.stringify({ query, variables }),
+  }).then((response) => response.json());
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,19 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000',
+        white: '#39C1CF',
+        grey: '#D9D9D9',
+        green: '#00984E',
+        paleGreen: '#E6F5F0',
+        yellow: '#EDC121',
+        blue: '#39C1CF',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
-
