60d5764e3c20e561b1d3b95fb4ffc728be0e04bd
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-06 11:49:29 -0500

    feat: add dev tools and .evn template

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,14 @@ import type { MetaFunction } from '@remix-run/node';
 import type { LinksFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
+import { withDevTools } from 'remix-development-tools';
+import rdtStylesheet from 'remix-development-tools/index.css';
 import tailwindStylesheet from './tailwind.css';
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: tailwindStylesheet }];
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: tailwindStylesheet },
+  ...(process.env.NODE_ENV === 'development' ? [{ rel: 'stylesheet', href: rdtStylesheet }] : []),
+];
 
 export const meta: MetaFunction = () => [
   {
@@ -14,7 +19,15 @@ export const meta: MetaFunction = () => [
   },
 ];
 
-export default function App() {
+let AppExport = App;
+
+if (process.env.NODE_ENV === 'development') {
+  AppExport = withDevTools(App);
+}
+
+export default AppExport;
+
+function App() {
   return (
     <html lang="en">
       <head>
