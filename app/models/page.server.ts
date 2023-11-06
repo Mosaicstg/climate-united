d628d285d1c3f3767bb480b0ae0488771dbbeb2e
JoeMosaic <joe@mosaicstg.com>
2023-11-06 16:52:41 -0500

    Create page.server.ts

---
+++ app/models/page.server.ts
@@ -0,0 +1,30 @@
+// query {
+//     page(id: "1ydvGd1x8TYHNWeNUbqFeC") {
+//         headline
+//         bodyText {
+//             json
+//         }
+//         featuredImage {
+//             fileName
+//             url
+//             description
+//             width
+//             height
+//         }
+//     }
+//     pageCollection(limit: 100) {
+//         items {
+//             headline
+//             bodyText {
+//                 json
+//             }
+//             featuredImage {
+//                 fileName
+//                 url
+//                 description
+//                 width
+//                 height
+//             }
+//         }
+//     }
+// }
