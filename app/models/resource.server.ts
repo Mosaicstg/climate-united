04af58458d1789fdcd33cda7097ea2df2cff0d10
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-07 15:41:35 -0500

    feat(resource): add server functions for querying Resource content type

--- app/models/resource.server.ts
+++ app/models/resource.server.ts
@@ -1,3 +1,7 @@
+import { fetchGraphQL } from "~/services/contentful.server";
+import { validateWithSchema } from "~/utils/validate-with-schema";
+import { z } from "zod";
+
 // query {
 //     resourceCollection(limit:100) {
 //         items {
@@ -25,3 +29,55 @@
 //     }
 // }
 // }
+
+const ResourceSchema = z.object({
+  title: z.string(),
+  file: z.object({
+    fileName: z.string(),
+    url: z.string(),
+  }),
+});
+
+const ResourcesSchema = ResourceSchema.array();
+
+type Resource = z.infer<typeof ResourceSchema>;
+
+export async function getResource(id: string): Promise<Resource> {
+  const query = `query {
+            resource(id: "${id}") {
+                title
+                file {
+                    fileName
+                    url
+                }
+            }
+        }`;
+
+  const response = await fetchGraphQL(query);
+  const resource = response.data.resource;
+
+  validateWithSchema(ResourceSchema, resource);
+
+  return resource;
+}
+
+export async function getResources(count: number = 10): Promise<Resource[]> {
+  const query = `query {
+        resourceCollection(limit: ${count}) {
+            items {
+                title
+                file {
+                    fileName
+                    url
+                }
+            }
+        }
+    }`;
+
+  const response = await fetchGraphQL(query);
+  const resources = response.data.resourceCollection.items;
+
+  validateWithSchema(ResourcesSchema, resources);
+
+  return resources;
+}
