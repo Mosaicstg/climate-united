44c3bd417566c9927d43b52846f627073b4541b3
JoeMosaic <joe@mosaicstg.com>
2023-11-07 16:06:30 -0500

    Create landing-page.server.ts

---
+++ app/models/landing-page.server.ts
@@ -0,0 +1,298 @@
+// query {
+//     landingPage(id: "5xt434uJ3Z3geyKLJ1iKPp") {
+//         heroText {
+//             json
+//         }
+//         heroImage {
+//             fileName
+//             url
+//             description
+//             width
+//             height
+//         }
+//         heroButton
+//         heroButtonUrl
+//         contentArea1Text {
+//             json
+//         }
+//         contentArea1ImagesCollection {
+//             items {
+//                 fileName
+//                 url
+//                 description
+//                 width
+//                 height
+//             }
+//         }
+//         contentArea2Text {
+//             json
+//         }
+//         contentArea2Image {
+//             fileName
+//             url
+//             description
+//             width
+//             height
+//         }
+//         contentArea3Headline
+//         contentArea3Text {
+//             json
+//         }
+//         contentArea4Image {
+//             fileName
+//             url
+//             description
+//             width
+//             height
+//         }
+//         contentArea4Text {
+//             json
+//         }
+//         eventsHeadline
+//         resourcesHeadline
+//         eventsResourcesImage {
+//             fileName
+//             url
+//             description
+//             width
+//             height
+//         }
+//         newsPressHeadline
+//     }
+// }
+
+// {
+//     "data": {
+//     "landingPage": {
+//         "heroText": {
+//             "json": {
+//                 "data": {},
+//                 "content": [
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "We understand the urgency of the climate crisis, we have one shot to get this right, and are ready to meet the moment.",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "paragraph"
+//                     }
+//                 ],
+//                     "nodeType": "document"
+//             }
+//         },
+//         "heroButton": "Meet Our Advisors",
+//             "heroButtonUrl": "/about-the-greenhouse-gas-reduction-fund",
+//             "contentArea1Text": {
+//             "json": {
+//                 "data": {},
+//                 "content": [
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "Calvert Impact, Community Preservation Corporation, and Self-Help have joined forces to form Climate United",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "heading-2"
+//                     },
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [
+//                                     {
+//                                         "type": "bold"
+//                                     }
+//                                 ],
+//                                 "value": "A coalition competing to manage an award from the $14 billion National Clean Investment Fund",
+//                                 "nodeType": "text"
+//                             },
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": ", a component of the Greenhouse Gas Reduction Fund (GGRF) established under the Inflation Reduction Act. Awards are expected to be announced in March.",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "paragraph"
+//                     }
+//                 ],
+//                     "nodeType": "document"
+//             }
+//         },
+//         "contentArea2Text": {
+//             "json": {
+//                 "data": {},
+//                 "content": [
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "Our work together - alongside hundreds of our community-based partners nationwide - will leverage public funds to transform markets in pursuit of a more inclusive net-zero transition.",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "heading-2"
+//                     },
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "Together we’ll deliver:",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "heading-3"
+//                     },
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "→ Better health\n→ Lower energy costs\n→ Quality jobs\n→ Equitable economic \n→ Opportunity for all",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "paragraph"
+//                     }
+//                 ],
+//                     "nodeType": "document"
+//             }
+//         },
+//         "contentArea3Headline": "Our Team Has",
+//             "contentArea3Text": {
+//             "json": {
+//                 "data": {},
+//                 "content": [
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "all with a strong track record  creating economic opportunity, sustainable development, and climate solutions in Justice40 communities.",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "heading-3"
+//                     },
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "paragraph"
+//                     }
+//                 ],
+//                     "nodeType": "document"
+//             }
+//         },
+//         "contentArea4Text": {
+//             "json": {
+//                 "data": {},
+//                 "content": [
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "Our work together - alongside hundreds of our community-based partners nationwide - that delivers better health, lower energy costs, quality jobs, and equitable economic opportunity for all.",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "heading-4"
+//                     },
+//                     {
+//                         "data": {},
+//                         "content": [
+//                             {
+//                                 "data": {},
+//                                 "marks": [],
+//                                 "value": "",
+//                                 "nodeType": "text"
+//                             }
+//                         ],
+//                         "nodeType": "paragraph"
+//                     }
+//                 ],
+//                     "nodeType": "document"
+//             }
+//         },
+//         "eventsHeadline": "Event Calendar",
+//             "resourcesHeadline": "GGRF Resources",
+//             "newsPressHeadline": "News and Press Releases",
+//             "heroImage": {
+//             "fileName": "Electric cooking.png",
+//                 "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                 "description": "Stir Fry. Yum!",
+//                 "width": 577,
+//                 "height": 603
+//         },
+//         "contentArea2Image": {
+//             "fileName": "Electric cooking.png",
+//                 "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                 "description": "Stir Fry. Yum!",
+//                 "width": 577,
+//                 "height": 603
+//         },
+//         "contentArea4Image": {
+//             "fileName": "Electric cooking.png",
+//                 "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                 "description": "Stir Fry. Yum!",
+//                 "width": 577,
+//                 "height": 603
+//         },
+//         "eventsResourcesImage": {
+//             "fileName": "Electric cooking.png",
+//                 "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                 "description": "Stir Fry. Yum!",
+//                 "width": 577,
+//                 "height": 603
+//         },
+//         "contentArea1ImagesCollection": {
+//             "items": [
+//                 {
+//                     "fileName": "Electric cooking.png",
+//                     "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                     "description": "Stir Fry. Yum!",
+//                     "width": 577,
+//                     "height": 603
+//                 },
+//                 {
+//                     "fileName": "Electric cooking.png",
+//                     "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                     "description": "Stir Fry. Yum!",
+//                     "width": 577,
+//                     "height": 603
+//                 },
+//                 {
+//                     "fileName": "Electric cooking.png",
+//                     "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+//                     "description": "Stir Fry. Yum!",
+//                     "width": 577,
+//                     "height": 603
+//                 }
+//             ]
+//         }
+//     }
+// }
+// }
