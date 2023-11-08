ccd3298b1eeeaaf5b5a1ab8b670b70eda98c272e
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-08 16:22:12 -0500

    fix: remove comments

--- app/models/team-member.server.ts
+++ app/models/team-member.server.ts
@@ -20,27 +20,6 @@ import { ImageSchema } from "~/schemas/contentful-fields/image.server"
 //     }
 // }
 
-// {
-//     "data": {
-//     "teamMemberCollection": {
-//         "items": [
-//             {
-//                 "name": "Caroline Nowery",
-//                 "position": "Sr. Vice President, Chief External Affairs Officer",
-//                 "department": "Virginia Community Capital",
-//                 "featuredImage": {
-//                     "fileName": "Electric cooking.png",
-//                     "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
-//                     "description": "Stir Fry. Yum!",
-//                     "width": 577,
-//                     "height": 603
-//                 }
-//             }
-//         ]
-//     }
-// }
-// }
-
 export const TeamMemberSchema = z.object({
   name: z.string(),
   position: z.string(),
