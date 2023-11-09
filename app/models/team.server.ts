35bdb1db37ebca79ecfffb22911d488e31914f5d
JoeMosaic <joe@mosaicstg.com>
2023-11-09 12:14:20 -0500

    server query fixes

--- app/models/team.server.ts
+++ app/models/team.server.ts
@@ -3,22 +3,26 @@
  *   teamPage(id: "r4OYblQ1BKEvh8k7RHp09") {
  *     title
  *     headline
- *     sections {
- *       title
- *       headline
- *       mainContent {
- *         json
- *       }
- *       teamMembers {
- *         name
- *         position
- *         department
- *         featuredImage {
- *           fileName
- *           url
- *           description
- *           width
- *           height
+ *     sectionsCollection {
+ *       items {
+ *         title
+ *         headline
+ *         mainContent {
+ *           json
+ *         }
+ *         teamMembersCollection(limit: 50) {
+ *           items {
+ *             name
+ *             position
+ *             department
+ *             featuredImage {
+ *               fileName
+ *               url
+ *               description
+ *               width
+ *               height
+ *             }
+ *           }
  *         }
  *       }
  *     }
@@ -32,35 +36,49 @@
  *     "teamPage": {
  *       "title": "Our Team",
  *       "headline": "Our Team",
- *       "sections": {
- *         "title": "Board of Directors",
- *         "headline": "Our Board of Directors",
- *         "mainContent": {
- *           "json": {
- *             "nodeType": "document",
- *             "data": {},
- *             "content": [
- *               {
- *                 "nodeType": "paragraph",
+ *       "sectionsCollection": {
+ *         "items": [
+ *           {
+ *             "title": "Board of Directors",
+ *             "headline": "Our Board of Directors",
+ *             "mainContent": {
+ *               "json": {
  *                 "data": {},
  *                 "content": [
  *                   {
- *                     "nodeType": "text",
- *                     "value": "Our Inaugural Board of Directors will oversee the consortium strategy and implementation of the Environmental Protection Agency’s $14 billion National Clean Investment Fund, part of the Greenhouse Gas Reduction Fund, if its proposal to manage all or part of the fund is chosen by the EPA in its ongoing selection process. The Board is comprised of leaders in clean energy, community and economic development, sustainable finance, and movement building.",
- *                     "marks": [],
- *                     "data": {}
+ *                     "data": {},
+ *                     "content": [
+ *                       {
+ *                         "data": {},
+ *                         "marks": [],
+ *                         "value": "Our Inaugural Board of Directors will oversee the consortium strategy and implementation of the Environmental Protection Agency’s $14 billion National Clean Investment Fund, part of the Greenhouse Gas Reduction Fund, if its proposal to manage all or part of the fund is chosen by the EPA in its ongoing selection process. The Board is comprised of leaders in clean energy, community and economic development, sustainable finance, and movement building.",
+ *                         "nodeType": "text"
+ *                       }
+ *                     ],
+ *                     "nodeType": "paragraph"
  *                   }
- *                 ]
+ *                 ],
+ *                 "nodeType": "document"
  *               }
- *             ]
+ *             },
+ *             "teamMembersCollection": {
+ *               "items": [
+ *                 {
+ *                   "name": "Caroline Nowery",
+ *                   "position": "Sr. Vice President, Chief External Affairs Officer",
+ *                   "department": "Virginia Community Capital",
+ *                   "featuredImage": {
+ *                     "fileName": "Electric cooking.png",
+ *                     "url": "https://images.ctfassets.net/urgx2rpiyypc/1TfWT4CCQK8lHE2FB95D0h/56f4543955afd7fe7a25e789369f8267/Electric_cooking.png",
+ *                     "description": "Stir Fry. Yum!",
+ *                     "width": 577,
+ *                     "height": 603
+ *                   }
+ *                 }
+ *               ]
+ *             }
  *           }
- *         },
- *         "teamMembers": {
- *           "name": "Caroline Nowery",
- *           "position": "Sr. Vice President, Chief External Affairs Officer",
- *           "department": "Virginia Community Capital",
- *           "featuredImage": null
- *         }
+ *         ]
  *       }
  *     }
  *   }
--- app/schemas/sections/section.team.server.ts
+++ app/schemas/sections/section.team.server.ts
@@ -6,16 +6,18 @@
  *     mainContent {
  *       json
  *     }
- *     teamMembers {
- *       name
- *       position
- *       department
- *       featuredImage {
- *         fileName
- *         url
- *         description
- *         width
- *         height
+ *     teamMembersCollection {
+ *       items {
+ *         name
+ *         position
+ *         department
+ *         featuredImage {
+ *           fileName
+ *           url
+ *           description
+ *           width
+ *           height
+ *         }
  *       }
  *     }
  *   }
@@ -30,29 +32,33 @@
  *       "headline": "Our Board of Directors",
  *       "mainContent": {
  *         "json": {
- *           "nodeType": "document",
  *           "data": {},
  *           "content": [
  *             {
- *               "nodeType": "paragraph",
  *               "data": {},
  *               "content": [
  *                 {
- *                   "nodeType": "text",
- *                   "value": "Our Inaugural Board of Directors will oversee the consortium strategy and implementation of the Environmental Protection Agency’s $14 billion National Clean Investment Fund, part of the Greenhouse Gas Reduction Fund, if its proposal to manage all or part of the fund is chosen by the EPA in its ongoing selection process. The Board is comprised of leaders in clean energy, community and economic development, sustainable finance, and movement building.",
+ *                   "data": {},
  *                   "marks": [],
- *                   "data": {}
+ *                   "value": "Our Inaugural Board of Directors will oversee the consortium strategy and implementation of the Environmental Protection Agency’s $14 billion National Clean Investment Fund, part of the Greenhouse Gas Reduction Fund, if its proposal to manage all or part of the fund is chosen by the EPA in its ongoing selection process. The Board is comprised of leaders in clean energy, community and economic development, sustainable finance, and movement building.",
+ *                   "nodeType": "text"
  *                 }
- *               ]
+ *               ],
+ *               "nodeType": "paragraph"
  *             }
- *           ]
+ *           ],
+ *           "nodeType": "document"
  *         }
  *       },
- *       "teamMembers": {
- *         "name": "Caroline Nowery",
- *         "position": "Sr. Vice President, Chief External Affairs Officer",
- *         "department": "Virginia Community Capital",
- *         "featuredImage": null
+ *       "teamMembersCollection": {
+ *         "items": [
+ *           {
+ *             "name": "Caroline Nowery",
+ *             "position": "Sr. Vice President, Chief External Affairs Officer",
+ *             "department": "Virginia Community Capital",
+ *             "featuredImage": null
+ *           }
+ *         ]
  *       }
  *     }
  *   }
