16c3e46056ee9a80cc0bbd6f3958e54e2d2b8a06
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-13 18:15:53 -0500

    feat: improve pattern for SEO meta tags

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,8 @@ import { json } from "@remix-run/node"
 import { getLandingPage } from "~/models/landing-page.server"
 import { invariantResponse } from "~/utils/invariant.server"
 import { LandingPage } from "~/ui/templates/LandingPage"
+import { getSocialMetas } from "~/utils/seo"
+import { type RootLoader } from "~/root"
 
 export const loader = async () => {
   const landingPage = await getLandingPage("2OVuAnTbko7I7V8nzbBR8K")
@@ -12,19 +14,21 @@ export const loader = async () => {
   return json({ landingPage })
 }
 
-export const meta: MetaFunction<typeof loader> = ({ data }) => {
+export const meta: MetaFunction<typeof loader, { root: RootLoader }> = ({
+  data,
+  matches,
+}) => {
+  const domainURL = matches.find((match) => match.id === "root")?.data
+    ?.domainURL
   return [
     ...(data
       ? [
-          { title: `${data.landingPage.title} - Climate United` },
-          {
-            name: "description",
-            content: `${data.landingPage.seo.excerpt}`,
-          },
-          {
-            property: "og:image",
-            content: `${data.landingPage.seo.image.url}`,
-          },
+          ...getSocialMetas({
+            title: data.landingPage.title,
+            image: data.landingPage.seo.image.url,
+            description: data.landingPage.seo.excerpt,
+            url: domainURL ?? "",
+          }),
         ]
       : []),
   ]
---
+++ app/utils/seo.ts
@@ -0,0 +1,32 @@
+export function getSocialMetas({
+  url,
+  title = "",
+  description = "",
+  image = "",
+  keywords = "",
+}: {
+  image?: string
+  url: string
+  title?: string
+  description?: string
+  keywords?: string
+}) {
+  return [
+    { title },
+    { name: "description", content: description },
+    { name: "keywords", content: keywords },
+    { name: "image", content: image },
+    { name: "og:url", content: url },
+    { name: "og:title", content: title },
+    { name: "og:description", content: description },
+    { name: "og:image", content: image },
+    {
+      name: "twitter:card",
+      content: image ? "summary_large_image" : "summary",
+    },
+    { name: "twitter:title", content: title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:image", content: image },
+    { name: "twitter:image:alt", content: title },
+  ]
+}
