038a2704b869704ec330bdda0339d2517063b93c
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-15 11:26:18 -0500

    feat: add framer motion

--- app/ui/templates/Page.tsx
+++ app/ui/templates/Page.tsx
@@ -2,6 +2,7 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { richTextRenderOptions } from "~/routes/about-the-greenhouse-gas-reduction-fund"
 import { type Page } from "~/models/page.server"
 import Header from "~/ui/components/Header"
+import { motion } from "framer-motion"
 
 type PageProps = Page
 
@@ -18,7 +19,16 @@ export function Page({
       <main>
         <div className="mx-auto max-w-screen-xl px-6 py-12 md:px-0">
           <div className="flex flex-col items-center gap-12 md:flex-row">
-            <div className="relative my-12 md:order-2 md:w-1/3">
+            <motion.div
+              initial={{ opacity: 0, transform: "scale(0)" }}
+              whileInView={{ opacity: 1, transform: "scale(1)" }}
+              transition={{
+                ease: "linear",
+                duration: 1.25,
+                x: { duration: 1 },
+              }}
+              className="relative my-12 md:order-2 md:w-1/3"
+            >
               <div className="right-3/5 absolute bottom-0 h-[156px] w-[156px] translate-x-1/2 translate-y-1/3 rounded-full bg-lightGreen"></div>
               <div className="absolute right-1/4 top-0 h-[156px] w-[156px] -translate-y-1/2 translate-x-1/2 rounded-full bg-blue"></div>
               <img
@@ -28,7 +38,7 @@ export function Page({
                 width={width}
                 height={height}
               />
-            </div>
+            </motion.div>
             <div className="text-darkBlue md:order-1 md:w-2/3">
               <h1 className="mb-5 text-3xl font-bold">{headline}</h1>
               {documentToReactComponents(
