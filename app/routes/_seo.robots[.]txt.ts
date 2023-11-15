939ce46d7fd93d0d8a7250df436b9d0d7d81bb70
Cesar Hernandez <cesar@mosaicstg.com>
2023-11-15 09:19:31 -0500

    Update _seo.robots[.]txt.ts

--- app/routes/_seo.robots[.]txt.ts
+++ app/routes/_seo.robots[.]txt.ts
@@ -5,7 +5,9 @@ import { getDomainUrl } from "~/utils/get-route-url.server"
 export function loader({ request }: DataFunctionArgs) {
   return generateRobotsTxt(
     [
+      // TODO: Uncomment this when we have a sitemap
       // { type: "sitemap", value: `${getDomainUrl(request)}/sitemap.xml` },
+      // TODO: remove before launch
       { type: "userAgent", value: "*" },
       { type: "disallow", value: "/" },
     ],
