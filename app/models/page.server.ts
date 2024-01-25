0a67f36a6ba11114d03938291bd3a012c404f603
Cesar Hernandez <cesar@mosaicstg.com>
2024-01-25 14:12:35 -0500

    feat: add SEO keywords to pages

--- app/models/page.server.ts
+++ app/models/page.server.ts
@@ -1,55 +1,21 @@
-import { fetchGraphQL } from "~/services/contentful.server"
+import { typedFetchGraphQL } from "~/services/contentful.server"
 import { z } from "zod"
 import { validateWithSchema } from "~/utils/validate-with-schema.server"
 import { RichTextSchema } from "~/schemas/contentful-fields/rich-text.server"
 import { ImageSchema } from "~/schemas/contentful-fields/image.server"
-
-// query {
-//     page(id: "1ydvGd1x8TYHNWeNUbqFeC") {
-//         headline
-//         mainContent {
-//             json
-//         }
-//         featuredImage {
-//             fileName
-//             url
-//             description
-//             width
-//             height
-//         }
-//     }
-//     pageCollection(limit: 100) {
-//         items {
-//             headline
-//             mainContent {
-//                 json
-//             }
-//             featuredImage {
-//                 fileName
-//                 url
-//                 description
-//                 width
-//                 height
-//             }
-//         }
-//     }
-// }
+import { SEO } from "./seo.server"
 
 export const PageSchema = z.object({
   title: z.string(),
   headline: z.string(),
   mainContent: RichTextSchema,
   featuredImage: ImageSchema,
-  seo: z.object({
-    title: z.string(),
-    excerpt: z.string(),
-    image: ImageSchema,
-  }),
+  seo: SEO
 })
 
 export type Page = z.infer<typeof PageSchema>
 
-export async function getPage(id: string): Promise<Page> {
+export async function getPage(id: string) {
   const query = `
         query {
             page(id: "${id}") {
@@ -75,12 +41,20 @@ export async function getPage(id: string): Promise<Page> {
                     width
                     height
                   }
+                  keywords
                 }
             }
         }
     `
 
-  const response = await fetchGraphQL(query)
+  const response = await typedFetchGraphQL<{ page: Page }>(query)
+
+  if (!response.data) {
+    console.error(`Error for Page with id:${id}`, response.errors)
+
+    return null
+  }
+
   const page = response.data.page
 
   return validateWithSchema(PageSchema, page)
