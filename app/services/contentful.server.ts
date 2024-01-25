b0349300e930af5a136225a618ad1b32e4b79fbb
Cesar Hernandez <cesar@mosaicstg.com>
2024-01-25 11:07:33 -0500

    feat: add ability to tap into separate Contenful environments

--- app/services/contentful.server.ts
+++ app/services/contentful.server.ts
@@ -6,7 +6,7 @@ export function fetchGraphQL(
   preview = false,
 ): Promise<any> {
   return fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}`,
     {
       method: "POST",
       headers: {
@@ -28,7 +28,7 @@ export async function typedFetchGraphQL<T>(
   preview = false,
 ): Promise<{ data: T } | { data: null; errors: Array<{ message: string }> }> {
   const response = await fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT}`,
     {
       method: "POST",
       headers: {
