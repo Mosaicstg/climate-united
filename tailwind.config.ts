d71cb0032818b7a4aca431d4798ad5eb3e17a6dc
Cesar Hernandez <cesar@mosaicstg.com>
2024-02-05 16:19:07 -0500

    feat: add shadcn/ui components for Modal, ScrollArea, Select, fakerjs for mocking

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,17 @@
 import type { Config } from "tailwindcss"
 
-export default {
+const config = {
+  darkMode: ["class"],
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         black: "#000",
@@ -15,7 +24,22 @@ export default {
         blue: "#39C1CF",
         darkBlue: "#1A445F",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
 } satisfies Config
+
+export default config
