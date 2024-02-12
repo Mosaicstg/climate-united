0c4524fbc27b120e4415645d7255aa44e9392c43
Cesar Hernandez <cesar@mosaicstg.com>
2024-02-12 17:41:55 -0500

    feat: add regions data to home router loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,13 +5,16 @@ import { invariantResponse } from "~/utils/invariant.server"
 import { LandingPage } from "~/ui/templates/LandingPage"
 import { getSocialMetas } from "~/utils/seo"
 import { type RootLoader } from "~/root"
+import { getEPARegions } from "~/models/epa-region.server"
 
 export const loader = async () => {
   const landingPage = await getLandingPage("2OVuAnTbko7I7V8nzbBR8K")
 
   invariantResponse(landingPage, "Homepage not found.", { status: 404 })
 
-  return json({ landingPage })
+  const regions = await getEPARegions()
+
+  return json({ landingPage, regions } as const)
 }
 
 export const meta: MetaFunction<typeof loader, { root: RootLoader }> = ({
