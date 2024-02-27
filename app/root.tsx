fda60a24f7105f4024f327b50aef14a6f8bf0e52
Cesar Hernandez <cesar@mosaicstg.com>
2024-02-27 14:32:31 -0500

    fix: update Netlify caching

--- app/root.tsx
+++ app/root.tsx
@@ -119,8 +119,11 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 
 export const headers: HeadersFunction = ({ loaderHeaders }) => {
   return {
-    "Cache-Control":
-      "public, max-age=600, must-revalidate, s-maxage=600, stale-while-revalidate=3600",
+    // Tell the browser to always check the freshness of the cache
+    "Cache-Control": "public, max-age=0, must-revalidate",
+    // Tell the CDN to treat it as fresh for 5 minutes, but for a week after that return a stale version while it revalidates
+    "Netlify-CDN-Cache-Control":
+      "public, s-maxage=300, stale-while-revalidate=604800",
     ...loaderHeaders,
   }
 }
