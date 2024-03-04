7f72ce3db39394ad8c866410a491f3d7804a35af
Cesar Hernandez <cesar@mosaicstg.com>
2024-03-04 15:19:54 -0500

    fix: modify Vite config and update Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
+import { vitePlugin as remix } from "@remix-run/dev"
 import { installGlobals } from "@remix-run/node"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { remixDevTools } from "remix-development-tools/vite"
@@ -12,11 +12,14 @@ installGlobals()
 export default defineConfig({
   plugins: [
     remixDevTools(),
-    remix(),
     tsconfigPaths(),
-    netlifyPlugin(),
     envOnly(),
-    visualizer({ emitFile: true }),
+    remix(),
+    netlifyPlugin(),
+    visualizer({
+      emitFile: true,
+      gzipSize: true,
+    }),
   ],
   server: {
     port: 4321,
